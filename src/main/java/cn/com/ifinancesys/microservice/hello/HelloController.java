398a6af19e4d232065ff31b49b71ca6dfe1c0687
华宇 <hylbx2010@gmail.com>
2017-08-20 14:53:44 +0800

    Simple Spring Boot

---
+++ src/main/java/cn/com/ifinancesys/microservice/hello/HelloController.java
@@ -0,0 +1,14 @@
+package cn.com.ifinancesys.microservice.hello;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping(method = RequestMethod.GET, path = "hello")
+    public String hello() {
+        return "Hello";
+    }
+}
